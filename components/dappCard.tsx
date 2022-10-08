308ca0171f104749b1ccb59cec45b7b05e4dc9ef
David Weisiger <dweisiger@gmail.com>
2022-10-07 23:40:30 -0500

    chore: cleanup

--- components/dappCard.tsx
+++ components/dappCard.tsx
@@ -1,19 +1,20 @@
-export const DappCard = () => {
+export interface DappCardProps {
+  imgSrc: string;
+  name: string;
+  rating: number;
+}
+
+export const DappCard = ({ imgSrc, name, rating }: DappCardProps) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card bg-base-100 shadow-xl">
       <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img src={imgSrc} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
+          {name}
+          <div className="badge badge-secondary">{rating}</div>
         </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
-        </div>
       </div>
     </div>
   );
--- components/dappList.tsx
+++ components/dappList.tsx
@@ -1,10 +1,18 @@
+import { DappCard, DappCardProps } from "./dappCard";
+
+const dappInfoSample: DappCardProps = {
+  imgSrc: "https://placeimg.com/400/225/arch",
+  name: "Call of Duty Mobile Season 7",
+  rating: 4.5,
+};
+
 export const DappList = () => {
   return (
     <>
       <ul>
-        <li>Coffee</li>
-        <li>Tea</li>
-        <li>Milk</li>
+        <DappCard {...dappInfoSample} />
+        <DappCard {...dappInfoSample} />
+        <DappCard {...dappInfoSample} />
       </ul>
     </>
   );
